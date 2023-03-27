4b15cc968f362a7e49af4a4e3b777fb8c37eb65d
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-03-27 15:18:41 -0300

    chore: add heroicons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <DocumentMagnifyingGlassIcon className='h-64 w-64 text-indigo-600/20' />
+    </div>
   )
 }
