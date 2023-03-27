e47f1f8a95713f77fdeccb42d905c1c2fff5b5c8
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-03-27 15:24:30 -0300

    feat: building initial home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,18 @@ import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 export default function Home() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center'>
       <DocumentMagnifyingGlassIcon className='h-64 w-64 text-indigo-600/20' />
+
+      <h1 className='text-3xl text-center mt-2 text-black font-bold mb-5'>
+        Welcome to the Amazon Web Scraper
+      </h1>
+
+      <h2 className='text-lg italic text-center text-black/50'>Project to improve my skills</h2>
+
+      <h3 className='text-lg italic text-center text-black/50'>
+        https://github.com/leonardohsilvadev
+      </h3>
     </div>
   )
 }
