42817279cbe388f2960e24a382135249ffa7d36c
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-03-28 09:26:15 -0300

    feat: building sidebar component layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from '@/components'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='flex bg-[#F7FBFF] h-screen'>
+        <Sidebar />
+
+        <main className='p-10 max-w-7xl w-full mx-auto overflow-y-auto'>
+        {children}
+        </main>
+        </body>
     </html>
   )
 }
